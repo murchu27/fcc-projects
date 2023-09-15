1d64c0cd93e949a4fcbcb8922dfe0273a09304d4
murchu27 <m.b.murphy15@gmail.com>
2023-09-15 17:53:49 -0400

    Module 4-1

---
+++ csharp/4_variables/1_datatypes/Program.cs
@@ -0,0 +1,27 @@
+﻿// Unit 3
+// // signed ints
+// Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
+// Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
+// Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
+// Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
+
+// // unsigned ints
+// Console.WriteLine($"byte  : {byte.MinValue} to {byte.MaxValue}");
+// Console.WriteLine($"ushort  : {ushort.MinValue} to {ushort.MaxValue}");
+// Console.WriteLine($"uint    : {uint.MinValue} to {uint.MaxValue}");
+// Console.WriteLine($"ulong   : {ulong.MinValue} to {ulong.MaxValue}");
+
+
+
+// Unit 4
+// // floats
+// Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
+// Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
+// Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
+
+
+
+// Unit 5
+// reference type variables
+int[] data = new int[3];
+string myString = "Hello, World!";
